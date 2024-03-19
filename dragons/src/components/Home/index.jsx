cabf55a664af1cc526cc7cc68a63c4747f871b63
jvpf <jvictorpf@outlook.com>
2024-03-19 13:52:51 -0300

    feat: link to create page

--- dragons/src/components/Home/index.jsx
+++ dragons/src/components/Home/index.jsx
@@ -1,5 +1,6 @@
-import { DragonList } from "../DragonList"
-import { Header } from "../Header"
+import { DragonList } from "../DragonList";
+import { Header } from "../Header";
+import { Link } from 'react-router-dom';
 
 export function Home() {
     return (
@@ -9,7 +10,9 @@ export function Home() {
         <main>
           <section>
             <h2>Gostaria de adicionar um novo dragão?</h2>
+            <Link to='/create'>
             <button>Adicionar</button>
+            </Link>
           </section>
           <DragonList/>
         </main>
