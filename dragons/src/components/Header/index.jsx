a83930ce965141c081d089299a45004fba3a0697
jvpf <jvictorpf@outlook.com>
2024-03-21 09:31:34 -0300

    feat: add tailwind header

--- dragons/src/components/Header/index.jsx
+++ dragons/src/components/Header/index.jsx
@@ -3,8 +3,8 @@ import dragonIcon from "../../assets/dragon32x32.png";
 export function Header() {
     return (
       <>
-        <header>
-            <div>
+        <header className="w-full bg-zinc-900 h-14">
+            <div className="flex items-center justify-around pt-2.5">
                 <h1>Dragons</h1>
                 <img src={dragonIcon} alt="Dragon Icon" />
             </div>
